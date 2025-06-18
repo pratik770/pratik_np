0daa3a29b9431519a84218304f2df3e63f102698
Pratik piya <pratikpiya@yahoo.com>
2025-06-18 15:58:24 +0545

    ssa

--- BJ.js
+++ BJ.js
@@ -39,3 +39,59 @@ function shuffleDeck() {
     console.log(deck);
     console.log(sdeck);
 }
+
+function start(){
+
+    console.log("Starting...");
+    document.getElementById("deal").style.display = "none";
+    buildDeck();
+    dealhand();
+    document.getElementById("start").disabled = true; // Disable start button after starting the game
+    document.getElementById("deal").disabled = false; // Enable deal button initially
+    // document.getElementById("reset").disabled = false; // Enable reset button initially
+
+}
+
+function dealhand() {
+    console.log("dealhand called");
+    dealing = true; // Set dealing to true
+
+    newgame();
+    document.getElementById("action-buttons").style.display = "inline";
+    document.getElementById("deal").style.display = "none"; // Hide deal button
+
+    for (let i = 0; i < 2; i++) {
+
+        turn = "player"; // Set turn to player
+        playerHand.push(deck[0]);
+        ptotal += getValue(deck[0]); // Get the value of the card
+        console.log("Player Ace: " + pAce);
+        if (pAce === true){
+            checkBJ(); // Check if player has a blackjack
+            document.getElementById("your-sum").innerText = ptotal + "/" + (ptotal + 10); // Show both values of Ace
+        }
+        else{
+            document.getElementById("your-sum").innerText = ptotal;
+        }
+        console.log("Deal hand to player: " + ptotal);
+        console.log("Player Hand: ", playerHand);
+        deck.shift(); // Remove the dealt cards from the deck
+
+
+        turn = "dealer"; // Set turn to dealer
+        dealerHand.push(deck[0]);
+        dtotal += getValue(deck[0]); // Get the value of the card
+        console.log("Dealer Ace: " + dAce);
+        document.getElementById("dealer-sum").innerText = getValue(dealerHand[0]);
+        console.log("Dealer Hand: ", dealerHand);
+        console.log("Deal hand to dealer: " + dtotal);
+        deck.shift(); // Remove the dealt cards from the deck
+        
+    }
+    
+    showcards(); // Show the cards after dealing
+
+    console.log("remaining deck: ");
+    console.log(deck);
+    dealing = false; // Set dealing to false
+}
